1e502b089c98521257f7930fbb6f84b36b7518ee
Andrés Carrasquilla <Andres@192.168.1.10>
2016-08-03 09:46:36 -0500

    tercer intento

---
+++ Steach 1.1/src/java/Modelo/Conexion.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Andres
+ */
+public class Conexion {
+    private Statement consulta;
+    private ResultSet resultado;
+    private PreparedStatement enunciado;
+    private Connection conex;
+        
+}
+
---
+++ Steach 1.1/src/java/Modelo/UsuarioDAO.java
@@ -0,0 +1,13 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Andres
+ */
+//Esta es la clase donde estan todos los metodos de cliente, los metos deben ser llamados desde el controlador
+
+public class UsuarioDAO {
+    
+    
+}
