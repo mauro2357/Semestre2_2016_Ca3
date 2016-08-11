814cb4aade4702d473f92a8c443ae8feec9d7848
1304julian <julian.797@hotmail.com>
2016-08-11 15:56:01 -0500

    Insertar Habilidades

--- Steach 1.1/src/java/Controlador/regisHabilid.java
+++ Steach 1.1/src/java/Controlador/regisHabilid.java
@@ -31,18 +31,42 @@ public class regisHabilid extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String hab_fisica = request.getParameter("txtb_fisica");
-            String hab_Quimica = request.getParameter("txtb_Quimica");
-            String hab_Matematicas = request.getParameter("txtb_Matematicas");
-            String hab_Programacion = request.getParameter("txtb_Programacion");
-            String hab_Biologia = request.getParameter("txtb_Biologia");
-            String hab_Estadistica = request.getParameter("txtb_Estadistica");
-            String hab_Espanol = request.getParameter("txtb_Espanol");
+            String hab_Quimica = (String)request.getParameter("txtb_Quimica");
+            String hab_Matematicas = (String)request.getParameter("txtb_Matematicas");
+            String hab_Programacion = (String)request.getParameter("txtb_Programacion");
+            String hab_Biologia = (String)request.getParameter("txtb_Biologia");
+            String hab_Estadistica = (String)request.getParameter("txtb_Estadistica");
+            String hab_Espanol = (String)request.getParameter("txtb_Espanol");
             
             String usu_correo=request.getParameter("lbl_correo");
             Usuario usu = new Usuario();
             UsuarioDAO usudao= new UsuarioDAO();
             usu = usudao.verificarUsuario(usu_correo);
-            System.out.println(usu_correo);
+            if(hab_Quimica == null)
+                hab_Quimica="0";
+            else 
+                hab_Quimica="1";
+            if(hab_Matematicas == null) 
+                hab_Matematicas="0";
+            else
+                hab_Matematicas="1";
+            if(hab_Programacion == null)
+                hab_Programacion="0";
+            else
+                hab_Programacion="1";
+            if(hab_Biologia == null) 
+                hab_Biologia="0";
+            else
+                hab_Biologia="1";
+            if(hab_Estadistica == null)
+                hab_Estadistica="0";
+            else
+                hab_Estadistica="1";
+            if(hab_Espanol == null)
+                hab_Espanol="0";
+            else
+                hab_Espanol="1";
+            
             Habilidad hab = new Habilidad(usu_correo, hab_fisica, hab_Quimica, hab_Matematicas, hab_Biologia, hab_Estadistica, hab_Programacion, hab_Espanol);
             HabilidadDAO habDAO = new HabilidadDAO();
             habDAO.registrarHabilidades(hab);
--- Steach 1.1/src/java/Modelo/HabilidadDAO.java
+++ Steach 1.1/src/java/Modelo/HabilidadDAO.java
@@ -22,14 +22,20 @@ public class HabilidadDAO {
     public HabilidadDAO(){
         conex= new Conexion();
     }
-    
+//    INSERT INTO habilidades (hab_matematica, hab_fisica, hab_quimica, hab_programacion, 
+//            hab_biologia, hab_estadistica, hab_español,
+//            hab_calificacion, usu_correo) VALUES ('1', '1', '1', '1', '1', '1', '1', '1', 'julian@julian.com');
+//    
     public void registrarHabilidades(Habilidad hab){
-        
         try {
             Statement estatuto = conex.getConnection().createStatement();
-            estatuto.executeUpdate("INSERT INTO db_steach.habilidades VALUES('"+hab.getHab_matematicas()+"', '"
-                            +hab.getHab_fisica()+"', '"+hab.getHab_quimica()
-                            +"', '"+hab.getHab_programacion()+"', '"+hab.getHab_biologia()+"', '"+hab.getHab_estadistica()+"', '"+hab.getHab_espanol()+"', '-1', '"+hab.getUsu_correo()+"')");
+            estatuto.executeUpdate("INSERT INTO habilidades (hab_matematica, hab_fisica, "
+                    + "hab_quimica, hab_programacion, \n" +
+                "hab_biologia, hab_estadistica, hab_español," +
+                "hab_calificacion, usu_correo) VALUES ('"+hab.getHab_matematicas()+"','"+hab.getHab_fisica()+
+                        "','"+hab.getHab_quimica()+"','"+hab.getHab_programacion()+"','"+
+                    hab.getHab_biologia()+"','"+hab.getHab_estadistica()+"','"+hab.getHab_espanol()+
+                    "','-1'"+hab.getUsu_correo()+"');");
             estatuto.close();
             conex.desconectar();
 
